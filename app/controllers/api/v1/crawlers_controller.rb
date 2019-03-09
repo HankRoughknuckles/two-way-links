f6b81d7b0fa1c8c4028732d7ecc39a4c9001e7cf
Thomas Morris <tmorris@salsitasoft.com>
2019-03-09 23:45:26 +0100

    Make controller call scraper (not recursive)

--- app/controllers/api/v1/crawlers_controller.rb
+++ app/controllers/api/v1/crawlers_controller.rb
@@ -5,26 +5,7 @@ require 'mechanize'
 
 class Api::V1::CrawlersController < ActionController::API
   def create
-    # Resque.enqueue(SinglePageScraper, params[:target_url])
-
     resource = Resource.find_or_create_by_url(params[:target_url])
-
-    scrape_starting_with(resource.id)
-  end
-
-  def scrape_starting_with(resource_id)
-    resource = Resource.find(resource_id)
-    # agent = Mechanize.new.agent
-    page = Mechanize.new.get(resource.url)
-    page.links.each do |link|
-      puts "link = #{link.href} = #{link.text}"
-      resource = Resource.find_or_create_by_url(link.href)
-
-      # enqueue(resource.id)
-
-      # Register that destination in the db
-      # Make the resource that’s being targeted as a resource on the website
-      # Register the source page in the target as being one of the “subscribers"
-    end
+    Resque.enqueue(SinglePageScraper, resource.id)
   end
 end
--- lib/workers/single_page_scraper.rb
+++ lib/workers/single_page_scraper.rb
@@ -3,13 +3,17 @@ require 'mechanize'
 class SinglePageScraper
   @queue = :crawler_queue
 
-  # def self.perform(target_url)
-  #   resource = Resource.find(resource_id)
-  #   page = Mechanize.new.get(resource.url)
-  #   page.links.each do |link|
-  #     puts "link = #{link.href} = #{link.text}"
-  #     resource = Resource.find_or_create_by_url(link.href)
-  #   end
-  # end
+  def self.perform(resource_id)
+    resource = Resource.find(resource_id)
+    page = Mechanize.new.get(resource.url)
+
+    page.links.each do |link|
+      puts "link = #{link.href} = #{link.text}"
+      target = Resource.find_or_create_by_url(link.href)
+      resource.create_target_link_to(target)
+
+      # enqueue(target.id)
+    end
+  end
 
 end
